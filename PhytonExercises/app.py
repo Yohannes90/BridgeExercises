a6208a1d44fb49df4f90987a24d1ee3eef3df534
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-17 14:21:49 +0300

    Questiom 3

--- PhytonExercises/app.py
+++ PhytonExercises/app.py
@@ -24,3 +24,25 @@ class Questions:
         return new
     print("Question 1")
     print(new_list([45,34,5,34,6,346,34,6,457]))
+
+    #2. Write a function that takes a string as input and
+    #  returns a new string with all the vowels removed.
+    def vowelless(string_input):
+        vowels = ('a', 'e', 'i', 'o', 'u')
+        for i in string_input.lower():
+            if i in vowels:
+                string_input = string_input.replace(i,"")
+        return string_input
+    print("Question 2")
+    print(vowelless("try making THIS SENTENCE vowelless"))
+
+    #3. Write a function that takes a list of strings as input and
+    # returns a new list with all the strings that have a length of 4 or greater.
+    def four_strings(list_of_strings):
+        new_string = []
+        for i in list_of_strings:
+            if len(i) >= 4:
+                new_string.append(i)
+        return new_string
+    print("Question 3")
+    print(four_strings(["C#", "C++", "Python", "Java"]))
