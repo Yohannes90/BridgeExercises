a8b2c9f81e9857a63d467516e7b3ab2fe21aed5b
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-03-17 17:53:39 +0300

    Question (array)

---
+++ JavaExercises/Array.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+public class Array {
+    private int [] array;
+    private int size;
+    private int count = 0;
+    Array(int size){
+        this.size = size;
+        array = new int[size];
+    }
+    void addElement(int value) {
+        if (count == (size)) {
+            int[] temp = new int[size * 2];
+            System.arraycopy(array, 0, temp, 0, size);
+            array = temp;
+        }
+        array[count] = value;
+        count++;
+    }
+    void deleteElement(int index) {
+
+    }
+    void updateElement(int index, int value) {
+        array[index] = value;
+    }
+    void insertElement(int index, int value) {
+        int[] twmp = new int[size + 1];
+        for (int i = 0; i < index; i++)
+        {
+            twmp[i] = array[i];
+        }
+        twmp[index] = value;
+        for (int i = index; i < size + 1; i++)
+        {
+            twmp[i + 1] = array[i];
+        }
+        array = twmp;
+    }
+}
