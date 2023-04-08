625fd0f281926404912f14b1b33d1e619b350d3f
Yohannes90 <mekonnenyohannes90@gmail.com>
2023-04-08 20:09:54 +0300

    stack tests on stack main

--- JavaExercises/StackGeneric1/Main.java
+++ JavaExercises/StackGeneric1/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        GenericStack1<Integer> stack = new GenericStack1<>();
 
+        stack.push(767);
+        stack.push(897);
+        stack.push(97);
+        stack.push(82);
+        stack.push(100);
+
+        stack.printStack();
+
+        System.out.println("pop = " + stack.pop());
+        stack.printStack();
+
+        System.out.println("peek = " + stack.peek());
+        stack.printStack();
+
+        System.out.println("is Empty " + stack.isEmpty());
     }
 }
